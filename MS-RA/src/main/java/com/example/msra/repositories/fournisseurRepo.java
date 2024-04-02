9f35b8e968495f114a1e61d77b9c93b3f0d0a35b
AO <amine.oueslati@esprit.tn>
2024-04-02 23:32:06 +0100

    first commit

--- MS-RA/src/main/java/com/example/msra/repositories/fournisseurRepo.java
+++ MS-RA/src/main/java/com/example/msra/repositories/fournisseurRepo.java
@@ -1,5 +1,4 @@
 package com.example.msra.repositories;
-
 import com.example.msra.entities.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
