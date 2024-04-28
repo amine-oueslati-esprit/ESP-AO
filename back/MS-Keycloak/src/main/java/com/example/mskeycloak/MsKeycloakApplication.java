29320a48aef55f9faab1596ed250b5d2568cdb20
AO <amine.oueslati@esprit.tn>
2024-04-28 20:46:57 +0100

    adding  registry & gateway services

---
+++ back/MS-Keycloak/src/main/java/com/example/mskeycloak/MsKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.example.mskeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsKeycloakApplication.class, args);
+	}
+
+}
